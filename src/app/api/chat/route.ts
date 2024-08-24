d54dbf422c3c525ae33fa367b50d84b052c329aa
Philip Hardy <philiphardyj@gmail.com>
2024-08-24 08:22:57 -0400

    .env

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,16 +1,26 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText, convertToCoreMessages } from 'ai';
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export const dynamic = 'force-dynamic'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY!,
+});
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
 
-  const result = await streamText({
-    model: openai('gpt-4-turbo'),
-    messages: convertToCoreMessages(messages),
-  });
+    // Request the OpenAI API for the response based on the prompt
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        stream: true,
+        messages: messages,
+    });
 
-  return result.toDataStreamResponse();
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
 }
